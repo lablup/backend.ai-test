b67e87d7e57adc74a44a8c5d2f940e0f44a77c90
Joongi Kim <joongi@lablup.com>
2021-06-18 21:05:19 +0900

    feat: Implement some part of domain CRUD test
    * Use pexpect to interact with the CLI commands
    * Make a reference implementation for writing CLI tests

--- src/ai/backend/test/cli_integration/admin/test_domain.py
+++ src/ai/backend/test/cli_integration/admin/test_domain.py
@@ -8,48 +8,76 @@ As usual `backend.ai` commands, this script assumes that the environment variabl
 configured to access the manager API.
 """
 
-from subprocess import run
+import functools
+from pathlib import Path
+import re
 
-import click
+from ...utils.cli import run as _run, EOF
+
+_rs = functools.partial(re.search, flags=re.M)  # shortcut
 
 
-def test_domain():
+def test_domain(client_bin: Path, client_environ: dict[str, str]) -> None:
     print("[ Domain ]")
-    run([
-        'backend.ai', 'admin', 'domains', 'add',
+    run = functools.partial(_run, env=client_environ)
+
+    domain_name = 'testing'
+    p = run([
+        client_bin, 'admin', 'domains', 'add',
         '-i',
         '-d', 'test domain',
-        'testing',
+        domain_name,
     ])
-    run([
-        'backend.ai', 'admin', 'domains',
-    ])
-    run([
-        'backend.ai', 'admin', 'domains', 'update',
-        '--is-active', 'true',
-        '--total-resource-slots', '{"cpu":999,"mem":"999g"}',
-        '--allowed-vfolder-hosts', 'local:volume1',
-        '--allowed-vfolder-hosts', 'local:volume2',
-        '--allowed-docker-registries', 'cr.backend.ai',
-        'testing',
-    ])
-    run([
-        'backend.ai', 'admin', 'domains',
-    ])
-    run([
-        'backend.ai', 'admin', 'domains', 'delete',
-        'testing',
-    ])
-    run([
-        'backend.ai', 'admin', 'domains',
-    ])
-    run([
-        'backend.ai', 'admin', 'domains', 'purge',
-        'testing',
-    ])
-    run([
-        'backend.ai', 'admin', 'domains',
-    ])
+    p.expect("is created")
+    p.close()
+
+    try:
+        p = run([
+            client_bin, 'admin', 'domain', '-n', domain_name,
+        ])
+        p.expect(EOF)
+        assert _rs(f"^Name +{domain_name}".encode(), p.before)
+        assert _rs(rb"^Description +test domain", p.before)
+        assert _rs(rb"^Active\? +False", p.before)
+        p.close()
+
+        p = run([
+            client_bin, 'admin', 'domains',
+        ])
+        p.expect(EOF)
+        assert _rs(rb"^Name +Description +Active?", p.before)
+        assert _rs(f"^{domain_name} +test domain +False".encode(), p.before)
+        p.close()
+
+        run([
+            client_bin, 'admin', 'domains', 'update',
+            '--is-active', 'true',
+            '--total-resource-slots', '{"cpu":999,"mem":"999g"}',
+            '--allowed-vfolder-hosts', 'local:volume1',
+            '--allowed-vfolder-hosts', 'local:volume2',
+            '--allowed-docker-registries', 'cr.backend.ai',
+            domain_name,
+        ])
+        run([
+            client_bin, 'admin', 'domains',
+        ])
+        run([
+            client_bin, 'admin', 'domains', 'delete',
+            domain_name,
+        ])
+        run([
+            client_bin, 'admin', 'domains',
+        ])
+    finally:
+        p = run([
+            client_bin, 'admin', 'domains', 'purge',
+            domain_name,
+        ])
+        p.expect_exact("Are you sure?")
+        p.sendline("Y")
+        run([
+            client_bin, 'admin', 'domains',
+        ])
 
 
 def test_group():
