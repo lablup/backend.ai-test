cb2d1ecf8f2c01a10f4bbbcf691ff7c6633be85e
Joongi Kim <joongi@lablup.com>
2021-06-18 21:20:59 +0900

    docs: Update comment

--- src/ai/backend/test/cli_integration/admin/test_domain.py
+++ src/ai/backend/test/cli_integration/admin/test_domain.py
@@ -4,8 +4,8 @@ An integration test for the Backend.AI admin APIs.
 It runs and checks the result of a series of CRUD commands for various entities including domain, group,
 user, scaling group, resource policy, resource preset, etc.
 
-As usual `backend.ai` commands, this script assumes that the environment variables are correctly
-configured to access the manager API.
+The location of the client executable and the credential scripts are configured as environment variables
+read by the pytest fixtures in the ai.backend.test.cli_integration.conftest module.
 """
 
 import functools
