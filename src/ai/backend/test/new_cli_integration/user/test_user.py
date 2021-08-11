8106860be7c9d3ea2c74f9ddf0e6e9f71c16b2ae
Taewan Park <swimingtw@gmail.com>
2021-08-11 18:30:46 +0900

    setup: Add basic test structure code for user

---
+++ src/ai/backend/test/new_cli_integration/user/test_user.py
@@ -0,0 +1,23 @@
+from contextlib import closing
+import functools
+import json
+import re
+import sys
+
+import pytest
+
+from ...utils.cli import EOF, ClientRunnerFunc
+
+def test_add_user(run: ClientRunnerFunc):
+    print("[ Add user ]")
+    pass
+
+
+def test_update_user(run: ClientRunnerFunc):
+    print("[ Update user ]")
+    pass
+
+
+def test_delete_user(run: ClientRunnerFunc):
+    print("[ Delete user ]")
+    pass
