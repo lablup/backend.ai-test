1cdd98d3cb9a6645477dc4b94e1af0ce9f55382d
Taewan Park <swimingtw@gmail.com>
2021-08-12 14:37:04 +0900

    feat: Add additional checks for add_user testcase

--- src/ai/backend/test/new_cli_integration/user/test_user.py
+++ src/ai/backend/test/new_cli_integration/user/test_user.py
@@ -11,11 +11,22 @@ from ...utils.cli import EOF, ClientRunnerFunc
 def test_add_user(run: ClientRunnerFunc):
     print("[ Add user ]")
 
-    # Add user
-    add_arguments = ['--output=json', 'admin', 'user', 'add', '-u testaccount1', '-n "John Doe"', 
-                     '--need-password-change', 'default', 'testaccount1@lablup.com', '1q2w3e4r']
-    with closing(run(add_arguments)) as p:
+    # Check if test account exists
+    with closing(run(['--output=json', 'admin', 'user', 'list'])) as p:
         p.expect(EOF)
+        decoded = p.before.decode()
+        loaded = json.loads(decoded)
+        user_list = loaded.get('items')
+
+    test_user = get_user_from_json(user_list, 'testaccount1')
+
+    if not bool(test_user):
+        # Add user
+        add_arguments = ['--output=json', 'admin', 'user', 'add', '-u', 'testaccount1', '-n', 'John Doe',
+                         '--need-password-change', 'default', 'testaccount1@lablup.com', '1q2w3e4r']
+        with closing(run(add_arguments)) as p:
+            p.expect(EOF)
+            assert 'User testaccount1@lablup.com is created' in p.before.decode(), 'Account add error'
 
     # Check if user is added
     with closing(run(['--output=json', 'admin', 'user', 'list'])) as p:
