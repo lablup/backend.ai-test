53d618db242413fc7dbc640764ab3fe6b5c194aa
Taewan Park <swimingtw@gmail.com>
2021-08-12 15:25:20 +0900

    feat: Add role and status test case for add_user testcase

--- src/ai/backend/test/new_cli_integration/user/test_user.py
+++ src/ai/backend/test/new_cli_integration/user/test_user.py
@@ -18,15 +18,34 @@ def test_add_user(run: ClientRunnerFunc):
         loaded = json.loads(decoded)
         user_list = loaded.get('items')
 
-    test_user = get_user_from_json(user_list, 'testaccount1')
+    test_user1 = get_user_from_json(user_list, 'testaccount1')
+    test_user2 = get_user_from_json(user_list, 'testaccount2')
+    test_user3 = get_user_from_json(user_list, 'testaccount3')
 
-    if not bool(test_user):
+    if not bool(test_user1):
         # Add user
         add_arguments = ['--output=json', 'admin', 'user', 'add', '-u', 'testaccount1', '-n', 'John Doe',
                          '--need-password-change', 'default', 'testaccount1@lablup.com', '1q2w3e4r']
         with closing(run(add_arguments)) as p:
             p.expect(EOF)
             assert 'User testaccount1@lablup.com is created' in p.before.decode(), 'Account add error'
+
+    if not bool(test_user2):
+        # Add user
+        add_arguments = ['--output=json', 'admin', 'user', 'add', '-u', 'testaccount2', '-n', 'John Roe', '-r', 'admin',
+                         '-s', 'inactive', 'default', 'testaccount2@lablup.com', '1q2w3e4r']
+        with closing(run(add_arguments)) as p:
+            p.expect(EOF)
+            assert 'User testaccount2@lablup.com is created' in p.before.decode(), 'Account add error'
+
+    if not bool(test_user3):
+        # Add user
+        add_arguments = ['--output=json', 'admin', 'user', 'add', '-u', 'testaccount3', '-n', 'Richard Roe', '-r',
+                         'monitor', '-s', 'before-verification', '--need-password-change', 'default',
+                         'testaccount3@lablup.com', '1q2w3e4r']
+        with closing(run(add_arguments)) as p:
+            p.expect(EOF)
+            assert 'User testaccount3@lablup.com is created' in p.before.decode(), 'Account add error'
 
     # Check if user is added
     with closing(run(['--output=json', 'admin', 'user', 'list'])) as p:
@@ -35,13 +54,31 @@ def test_add_user(run: ClientRunnerFunc):
         loaded = json.loads(decoded)
         user_list = loaded.get('items')
 
-        assert isinstance(user_list, list), 'Expected user list'
-        added_user = get_user_from_json(user_list, 'testaccount1')
-        assert bool(added_user), 'Added account doesn\'t exist'
-        assert added_user.get('email') == 'testaccount1@lablup.com', 'E-mail mismatch'
-        assert added_user.get('full_name') == 'John Doe', 'Full name mismatch'
-        assert added_user.get('status') == 'active', 'User status mismatch'
-        assert added_user.get('need_password_change') == True, 'Password change status mismatch'
+    assert isinstance(user_list, list), 'Expected user list'
+    added_user1 = get_user_from_json(user_list, 'testaccount1')
+    added_user2 = get_user_from_json(user_list, 'testaccount2')
+    added_user3 = get_user_from_json(user_list, 'testaccount3')
+
+    assert bool(added_user1), 'Added account doesn\'t exist: Account#1'
+    assert added_user1.get('email') == 'testaccount1@lablup.com', 'E-mail mismatch: Account#1'
+    assert added_user1.get('full_name') == 'John Doe', 'Full name mismatch: Account#1'
+    assert added_user1.get('status') == 'active', 'User status mismatch: Account#1'
+    assert added_user1.get('role') == 'user', 'Role mismatch: Account#1'
+    assert added_user1.get('need_password_change') == True, 'Password change status mismatch: Account#1'
+
+    assert bool(added_user2), 'Added account doesn\'t exist: Account#2'
+    assert added_user2.get('email') == 'testaccount2@lablup.com', 'E-mail mismatch: Account#2'
+    assert added_user2.get('full_name') == 'John Roe', 'Full name mismatch: Account#2'
+    assert added_user2.get('status') == 'inactive', 'User status mismatch: Account#2'
+    assert added_user2.get('role') == 'admin', 'Role mismatch: Account#2'
+    assert added_user2.get('need_password_change') == False, 'Password change status mismatch: Account#2'
+
+    assert bool(added_user3), 'Added account doesn\'t exist: Account#3'
+    assert added_user3.get('email') == 'testaccount3@lablup.com', 'E-mail mismatch: Account#3'
+    assert added_user3.get('full_name') == 'Richard Roe', 'Full name mismatch: Account#3'
+    assert added_user3.get('status') == 'before-verification', 'User status mismatch: Account#3'
+    assert added_user3.get('role') == 'monitor', 'Role mismatch: Account#3'
+    assert added_user3.get('need_password_change') == True, 'Password change status mismatch: Account#3'
 
 
 def test_update_user(run: ClientRunnerFunc):
