bfce9cd1417f383fcef98683206f8c207105232c
Taewan Park <swimingtw@gmail.com>
2021-08-17 15:43:48 +0900

    feat: Add test for vfolder list

--- src/ai/backend/test/cli_integration/user/test_vfolder.py
+++ src/ai/backend/test/cli_integration/user/test_vfolder.py
@@ -17,4 +17,9 @@ def test_delete_vfolder(run: ClientRunnerFunc):
 
 
 def test_list_vfolder(run: ClientRunnerFunc):
-    pass
+    with closing(run(['--output=json', 'vfolder', 'list'])) as p:
+        p.expect(EOF)
+        decoded = p.before.decode()
+        loaded = json.loads(decoded)
+        folder_list = loaded.get('items')
+        assert isinstance(folder_list, list)
