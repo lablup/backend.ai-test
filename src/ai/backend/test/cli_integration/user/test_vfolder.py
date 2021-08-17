10f7a3eca4f4f1955815a0927a2222f23c5bff94
Taewan Park <swimingtw@gmail.com>
2021-08-17 15:27:16 +0900

    setup: Add basic structure code for vfolder test

--- src/ai/backend/test/cli_integration/user/test_vfolder.py
+++ src/ai/backend/test/cli_integration/user/test_vfolder.py
@@ -1,4 +1,20 @@
+import json
+from contextlib import closing
+
+from ...utils.cli import EOF, ClientRunnerFunc
 
 
-def test_x():
+def test_create_vfolder(run: ClientRunnerFunc):
+    pass
+
+
+def test_rename_vfolder(run: ClientRunnerFunc):
+    pass
+
+
+def test_delete_vfolder(run: ClientRunnerFunc):
+    pass
+
+
+def test_list_vfolder(run: ClientRunnerFunc):
     pass
