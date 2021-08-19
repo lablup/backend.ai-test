a7230a6676e7790fe7200b966e8d2b151892c004
Taewan Park <swimingtw@gmail.com>
2021-08-19 16:50:47 +0900

    setup: Add basic structure code for scaling group test

---
+++ src/ai/backend/test/cli_integration/admin/test_scaling_group.py
@@ -0,0 +1,20 @@
+import json
+from contextlib import closing
+
+from ...utils.cli import EOF, ClientRunnerFunc
+
+
+def test_add_scaling_group(run: ClientRunnerFunc):
+    pass
+
+
+def test_update_scaling_group(run: ClientRunnerFunc):
+    pass
+
+
+def test_delete_scaling_group(run: ClientRunnerFunc):
+    pass
+
+
+def test_list_scaling_group(run: ClientRunnerFunc):
+    pass
