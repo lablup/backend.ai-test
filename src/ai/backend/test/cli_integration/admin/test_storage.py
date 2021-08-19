eaa75cae50204b08a504651170fcc8c3cbd72f10
Taewan Park <swimingtw@gmail.com>
2021-08-19 15:40:30 +0900

    feat: Add test for list_storage

--- src/ai/backend/test/cli_integration/admin/test_storage.py
+++ src/ai/backend/test/cli_integration/admin/test_storage.py
@@ -5,7 +5,13 @@ from ...utils.cli import EOF, ClientRunnerFunc
 
 
 def test_list_storage(run: ClientRunnerFunc):
-    pass
+    print("[ List storage ]")
+    with closing(run(['--output=json', 'admin', 'storage', 'list'])) as p:
+        p.expect(EOF)
+        decoded = p.before.decode()
+        loaded = json.loads(decoded)
+        storage_list = loaded.get('items')
+        assert isinstance(storage_list, list), 'Storage list not printed properly'
 
 
 def test_info_storage(run: ClientRunnerFunc):
