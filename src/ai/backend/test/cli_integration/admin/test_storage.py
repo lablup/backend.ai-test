3176a19fce5c8f6f56fc76a7cc2e92e1ea0b9c02
Taewan Park <swimingtw@gmail.com>
2021-08-19 15:37:55 +0900

    setup: Add basic structure code for storage test

---
+++ src/ai/backend/test/cli_integration/admin/test_storage.py
@@ -0,0 +1,12 @@
+import json
+from contextlib import closing
+
+from ...utils.cli import EOF, ClientRunnerFunc
+
+
+def test_list_storage(run: ClientRunnerFunc):
+    pass
+
+
+def test_info_storage(run: ClientRunnerFunc):
+    pass
