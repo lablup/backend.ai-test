8cd6c1d0a32fe79cda01a2ca5bc40131290479a0
Taewan Park <swimingtw@gmail.com>
2021-08-19 11:04:57 +0900

    setup: Add basic structure code for keypair test

---
+++ src/ai/backend/test/cli_integration/admin/test_keypair.py
@@ -0,0 +1,20 @@
+import json
+from contextlib import closing
+
+from ...utils.cli import EOF, ClientRunnerFunc
+
+
+def test_add_keypair(run: ClientRunnerFunc):
+    pass
+
+
+def test_update_keypair(run: ClientRunnerFunc):
+    pass
+
+
+def test_delete_keypair(run: ClientRunnerFunc):
+    pass
+
+
+def test_list_keypair(run: ClientRunnerFunc):
+    pass
