c1877d24556f28510b02ed778cb5e448b4f7443f
Taewan Park <swimingtw@gmail.com>
2021-08-19 17:18:15 +0900

    feat: Add test code for add_scaling_group

--- src/ai/backend/test/cli_integration/admin/test_scaling_group.py
+++ src/ai/backend/test/cli_integration/admin/test_scaling_group.py
@@ -5,7 +5,28 @@ from ...utils.cli import EOF, ClientRunnerFunc
 
 
 def test_add_scaling_group(run: ClientRunnerFunc):
-    pass
+    # Create scaling group
+    with closing(run(['admin', 'scaling-group', 'add', '-d', 'Test scaling group', '-i', '--driver', 'static',
+                      '--scheduler', 'fifo', 'testgroup1'])) as p:
+        p.expect(EOF)
+        assert 'Scaling group name testgroup1 is created.' in p.before.decode(), \
+            'Test scaling group not created successfully'
+
+    # Check if scaling group is created
+    with closing(run(['--output=json', 'admin', 'scaling-group', 'list'])) as p:
+        p.expect(EOF)
+        decoded = p.before.decode()
+        loaded = json.loads(decoded)
+        scaling_group_list = loaded.get('items')
+        assert isinstance(scaling_group_list, list), 'Scaling group list not printed properly'
+
+    testgroup = get_scaling_group_from_list(scaling_group_list, 'testgroup1')
+
+    assert bool(testgroup), 'Test scaling group doesn\'t exist'
+    assert testgroup.get('description') == 'Test scaling group', 'Scaling group description mismatch'
+    assert testgroup.get('is_active') == False, 'Scaling group active status mismatch'
+    assert testgroup.get('driver') == 'static', 'Scaling group driver mismatch'
+    assert testgroup.get('scheduler') == 'fifo', 'Scaling group scheduler mismatch'
 
 
 def test_update_scaling_group(run: ClientRunnerFunc):
@@ -23,3 +44,11 @@ def test_list_scaling_group(run: ClientRunnerFunc):
         loaded = json.loads(decoded)
         scaling_group_list = loaded.get('items')
         assert isinstance(scaling_group_list, list), 'Scaling group list not printed properly'
+
+
+def get_scaling_group_from_list(scaling_groups: list, groupname: str) -> dict:
+    for sg in scaling_groups:
+        if sg.get('name') == groupname:
+            return sg
+
+    return {}
