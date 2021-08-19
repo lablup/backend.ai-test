aa9ff06473a7a12cc32ef18c54841bd983066965
Taewan Park <swimingtw@gmail.com>
2021-08-19 18:04:57 +0900

    feat: Add test code for update_scaling_group

--- src/ai/backend/test/cli_integration/admin/test_scaling_group.py
+++ src/ai/backend/test/cli_integration/admin/test_scaling_group.py
@@ -30,7 +30,29 @@ def test_add_scaling_group(run: ClientRunnerFunc):
 
 
 def test_update_scaling_group(run: ClientRunnerFunc):
-    pass
+    # Update scaling group
+    with closing(run(['admin', 'scaling-group', 'update', '-d', 'Test scaling group updated',
+                      '--driver', 'non-static', '--scheduler', 'lifo', 'testgroup1'])) as p:
+        p.expect(EOF)
+        assert 'Scaling group testgroup1 is updated.' in p.before.decode(), \
+            'Test scaling group not updated successfully'
+
+    # Check if scaling group is updated
+    with closing(run(['--output=json', 'admin', 'scaling-group', 'list'])) as p:
+        p.expect(EOF)
+        decoded = p.before.decode()
+        loaded = json.loads(decoded)
+        scaling_group_list = loaded.get('items')
+        assert isinstance(scaling_group_list, list), 'Scaling group list not printed properly'
+
+    testgroup = get_scaling_group_from_list(scaling_group_list, 'testgroup1')
+
+    assert bool(testgroup), 'Test scaling group doesn\'t exist'
+    assert testgroup.get('description') == 'Test scaling group updated', 'Scaling group description mismatch'
+    assert testgroup.get('is_active') == True, 'Scaling group active status mismatch'
+    assert testgroup.get('driver') == 'non-static', 'Scaling group driver mismatch'
+    assert testgroup.get('scheduler') == 'lifo', 'Scaling group scheduler mismatch'
+
 
 
 def test_delete_scaling_group(run: ClientRunnerFunc):
