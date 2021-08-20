68973847567cad69567ec47918178ddc7044b9a4
Taewan Park <swimingtw@gmail.com>
2021-08-20 11:16:53 +0900

    setup: Add basic structure code for keypair resource policy test

---
+++ src/ai/backend/test/cli_integration/admin/test_keypair_resource_policy.py
@@ -0,0 +1,20 @@
+import json
+from contextlib import closing
+
+from ...utils.cli import EOF, ClientRunnerFunc
+
+
+def test_add_keypair_resource_policy(run: ClientRunnerFunc):
+    pass
+
+
+def test_update_keypair_resource_policy(run: ClientRunnerFunc):
+    pass
+
+
+def test_delete_keypair_resource_policy(run: ClientRunnerFunc):
+    pass
+
+
+def test_list_keypair_resource_policy(run: ClientRunnerFunc):
+    pass
