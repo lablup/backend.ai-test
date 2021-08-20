ce3c774115687db023a5244160a045ca762a9f7a
Taewan Park <swimingtw@gmail.com>
2021-08-20 10:42:54 +0900

    feat: Add test for update_domain

--- src/ai/backend/test/cli_integration/admin/test_domain.py
+++ src/ai/backend/test/cli_integration/admin/test_domain.py
@@ -33,7 +33,33 @@ def test_add_domain(run: ClientRunnerFunc):
 
 
 def test_update_domain(run: ClientRunnerFunc):
-    pass
+    print("[ Update domain ]")
+
+    # Update domain
+    add_arguments = ['admin', 'domain', 'update', '--new-name', 'test123', '--description', 'Test domain updated',
+                     '--is-active', 'TRUE', '--total-resource-slots', '{}', '--allowed-vfolder-hosts', 'local:volume2',
+                     '--allowed-docker-registries', 'cr1.backend.ai', 'test']
+    with closing(run(add_arguments)) as p:
+        p.expect(EOF)
+        assert 'Domain test is updated.' in p.before.decode(), 'Domain update not successful'
+
+    # Check if domain is updated
+    with closing(run(['--output=json', 'admin', 'domain', 'list'])) as p:
+        p.expect(EOF)
+        decoded = p.before.decode()
+        loaded = json.loads(decoded)
+        domain_list = loaded.get('items')
+        assert isinstance(domain_list, list), 'Domain list not printed properly'
+
+    test_domain = get_domain_from_list(domain_list, 'test123')
+
+    assert bool(test_domain), 'Test domain doesn\'t exist'
+    assert test_domain.get('description') == 'Test domain updated', 'Domain description mismatch'
+    assert test_domain.get('is_active') == True, 'Domain active status mismatch'
+    assert test_domain.get('total_resource_slots') == '{}', 'Domain total resource slots mismatch'
+    assert test_domain.get('allowed_vfolder_hosts') == ['local:volume2'], 'Domain allowed vfolder hosts mismatch'
+    assert test_domain.get('allowed_docker_registries') == ['cr1.backend.ai'], \
+        'Domain allowed docker registries mismatch'
 
 
 def test_delete_domain(run: ClientRunnerFunc):
