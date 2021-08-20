ef15217c10ba225bb64e78a340751a692704a52a
Taewan Park <swimingtw@gmail.com>
2021-08-20 09:26:45 +0900

    setup: Add basic structure code for domain test

--- src/ai/backend/test/cli_integration/admin/test_domain.py
+++ src/ai/backend/test/cli_integration/admin/test_domain.py
@@ -7,101 +7,18 @@ import pytest
 
 from ...utils.cli import EOF, ClientRunnerFunc
 
-_rs = functools.partial(re.search, flags=re.M)  # shortcut
 
-
-@pytest.mark.dependency(name="domain_crud")
-def test_domain(domain_name: str, run: ClientRunnerFunc) -> None:
-    with closing(run([
-        'admin', 'domains', 'add',
-        '-i',
-        '-d', 'test domain',
-        domain_name,
-    ])) as p:
-        p.expect("is created")
-        p.expect(EOF)
-
-    try:
-        with closing(run([
-            'admin', 'domain', '-n', domain_name,
-        ])) as p:
-            p.expect(EOF)
-            assert _rs(f"^Name +{domain_name}".encode(), p.before)
-            assert _rs(rb"^Description +test domain", p.before)
-            assert _rs(rb"^Active\? +False", p.before)
-
-        with closing(run([
-            'admin', 'domains',
-        ])) as p:
-            p.expect(EOF)
-            assert _rs(rb"^Name +Description +Active\?", p.before)
-            assert _rs(f"^{domain_name} +test domain +False".encode(), p.before)
-
-        with closing(run([
-            'admin', 'domains', 'update',
-            '--is-active', 'true',
-            '--total-resource-slots', '{"cpu":999,"mem":"999g"}',
-            '--allowed-vfolder-hosts', 'local:volume1',
-            '--allowed-vfolder-hosts', 'local:volume2',
-            '--allowed-docker-registries', 'cr.backend.ai',
-            domain_name,
-        ])) as p:
-            p.expect("is updated")
-            p.expect(EOF)
-
-        with closing(run([
-            'admin', 'domain', '-n', domain_name,
-        ])) as p:
-            p.logfile = sys.stdout.buffer
-            p.expect(EOF)
-            assert _rs(rb"^Active\? +True", p.before)
-            assert _rs(rb"Total Resource Slots +{\"cpu\": ?\"999\",", p.before)
-            assert _rs(rb"\blocal:volume1\b", p.before)
-            assert _rs(rb"\blocal:volume2\b", p.before)
-            assert _rs(rb"\bcr\.backend\.ai\b", p.before)
-
-        with closing(run([
-            'admin', 'domains', 'delete',
-            domain_name,
-        ])) as p:
-            p.expect("is inactivated")
-            p.expect(EOF)
-
-    finally:
-        with closing(run([
-            'admin', 'domains', 'purge',
-            domain_name,
-        ])) as p:
-            p.expect_exact("Are you sure?")
-            p.sendline("Y")
-            p.expect(EOF)
-
-        with closing(run([
-            'admin', 'domains',
-        ])) as p:
-            p.expect(EOF)
-
-
-def test_user():
-    print("[ User ]")
+def test_add_domain(run: ClientRunnerFunc):
     pass
 
 
-def test_keypair():
-    print("[ KeyPair ]")
+def test_update_domain(run: ClientRunnerFunc):
     pass
 
 
-def test_scaling_group():
-    print("[ ScalingGroup ]")
+def test_delete_domain(run: ClientRunnerFunc):
     pass
 
 
-def test_resource_policy():
-    print("[ ResourcePolicy ]")
-    pass
-
-
-def test_resource_preset():
-    print("[ ResourcePreset ]")
+def test_list_domain(run: ClientRunnerFunc):
     pass
