75a569d4b3a58aebee79f7efa2388cc41487bc95
Taewan Park <swimingtw@gmail.com>
2021-08-20 10:58:15 +0900

    setup: Add basic structure code for image test

---
+++ src/ai/backend/test/cli_integration/admin/test_image.py
@@ -0,0 +1,16 @@
+import json
+from contextlib import closing
+
+from ...utils.cli import EOF, ClientRunnerFunc
+
+
+def test_alias_image(run: ClientRunnerFunc):
+    pass
+
+
+def test_dealias_image(run: ClientRunnerFunc):
+    pass
+
+
+def test_list_image(run: ClientRunnerFunc):
+    pass
