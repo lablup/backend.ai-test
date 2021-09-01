6e0978d35142802a734d5ffe13c61909084ac06e
Joongi Kim <me@daybreaker.info>
2021-09-01 13:07:49 +0900

    format: Fix trailing newline of files

--- src/ai/backend/test/cli_integration/user/test_vfolder.py
+++ src/ai/backend/test/cli_integration/user/test_vfolder.py
@@ -17,13 +17,13 @@ def test_create_vfolder(run: ClientRunnerFunc):
     #         'Test group not created successfully.'
     print("[ Create vfolder ]")
     # Create vfolder
-    with closing(run(['vfolder', 'create',  '-p', 'rw', 'testfolder1', 'local:volume1'])) as p:
+    with closing(run(['vfolder', 'create',  '-p', 'rw', 'test_folder1', 'local:volume1'])) as p:
         p.expect(EOF)
-        assert 'Virtual folder "testfolder1" is created' in p.before.decode(), 'Test folder1 not created successfully.'
+        assert 'Virtual folder "test_folder1" is created' in p.before.decode(), 'Test folder1 not created successfully.'
 
-    with closing(run(['vfolder', 'create', '-p', 'ro', 'testfolder2', 'local:volume1'])) as p:
+    with closing(run(['vfolder', 'create', '-p', 'ro', 'test_folder2', 'local:volume1'])) as p:
         p.expect(EOF)
-        assert 'Virtual folder "testfolder2" is created' in p.before.decode(), 'Test folder2 not created successfully.'
+        assert 'Virtual folder "test_folder2" is created' in p.before.decode(), 'Test folder2 not created successfully.'
 
     # Check if vfolder is created
     with closing(run(['--output=json', 'vfolder', 'list'])) as p:
@@ -33,14 +33,14 @@ def test_create_vfolder(run: ClientRunnerFunc):
         folder_list = loaded.get('items')
         assert isinstance(folder_list, list), 'Error in listing test folders!'
 
-    testfolder1 = get_folder_from_list(folder_list, 'testfolder1')
-    testfolder2 = get_folder_from_list(folder_list, 'testfolder2')
+    test_folder1 = get_folder_from_list(folder_list, 'test_folder1')
+    test_folder2 = get_folder_from_list(folder_list, 'test_folder2')
 
-    assert bool(testfolder1), 'Test folder 1 doesn\'t exist!'
-    assert testfolder1.get('permission') == 'rw', 'Test folder 1 permission mismatch.'
+    assert bool(test_folder1), 'Test folder 1 doesn\'t exist!'
+    assert test_folder1.get('permission') == 'rw', 'Test folder 1 permission mismatch.'
 
-    assert bool(testfolder2), 'Test folder 2 doesn\'t exist!'
-    assert testfolder2.get('permission') == 'ro', 'Test folder 2 permission mismatch.'
+    assert bool(test_folder2), 'Test folder 2 doesn\'t exist!'
+    assert test_folder2.get('permission') == 'ro', 'Test folder 2 permission mismatch.'
 
 
 def test_rename_vfolder(run: ClientRunnerFunc):
@@ -51,7 +51,7 @@ def test_rename_vfolder(run: ClientRunnerFunc):
     """
     print("[ Rename vfolder ]")
     # Rename vfolder
-    with closing(run(['vfolder', 'rename', 'testfolder1', 'testfolder3'])) as p:
+    with closing(run(['vfolder', 'rename', 'test_folder1', 'test_folder3'])) as p:
         p.expect(EOF)
         assert 'Renamed' in p.before.decode(), 'Test folder1 not renamed successfully.'
 
@@ -63,8 +63,8 @@ def test_rename_vfolder(run: ClientRunnerFunc):
         folder_list = loaded.get('items')
         assert isinstance(folder_list, list), 'Error in listing test folders!'
 
-    testfolder3 = get_folder_from_list(folder_list, 'testfolder3')
-    assert bool(testfolder3), 'Test folder 3 doesn\'t exist!'
+    test_folder3 = get_folder_from_list(folder_list, 'test_folder3')
+    assert bool(test_folder3), 'Test folder 3 doesn\'t exist!'
 
 
 def test_delete_vfolder(run: ClientRunnerFunc):
@@ -74,11 +74,11 @@ def test_delete_vfolder(run: ClientRunnerFunc):
     Otherwise, it will raise an error.
     """
     print("[ Delete vfolder ]")
-    with closing(run(['vfolder', 'delete', 'testfolder2'])) as p:
+    with closing(run(['vfolder', 'delete', 'test_folder2'])) as p:
         p.expect(EOF)
         assert 'Deleted' in p.before.decode(), 'Test folder 2 not deleted successfully.'
 
-    with closing(run(['vfolder', 'delete', 'testfolder3'])) as p:
+    with closing(run(['vfolder', 'delete', 'test_folder3'])) as p:
         p.expect(EOF)
         assert 'Deleted' in p.before.decode(), 'Test folder 3 not deleted successfully.'
 
@@ -99,6 +99,4 @@ def get_folder_from_list(folders: list, foldername: str) -> dict:
     for folder in folders:
         if folder.get('name', '') == foldername:
             return folder
-
     return {}
-
