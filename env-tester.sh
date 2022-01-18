bff0587e2fceb60d3c1d2ae21c6d5a73172da910
sh <oper6909@gmail.com>
2022-01-18 11:01:03 +0900

    chore: remove cache of gitignored file

--- env-tester.sh
+++
@@ -1,3 +0,0 @@
-# Set env to test backend.ai components through client-py.
-export BACKENDAI_TEST_CLIENT_VENV=/Users/sh/.pyenv/versions/venv-dev-client
-export BACKENDAI_TEST_CLIENT_ENV=/Users/sh/dev/client-py/env-local-admin-api.sh
